90184f4770841bb8d38a1fa03cef1874a93c8c62
4559jacky <4559jacky@gmail.com>
2025-02-06 16:01:08 +0900

    [jji] jsp 과제 commit
    	- 도서 대출 시스템 과제 완료
    	- 정보를 입력하여 보낸 데이터를 다음 페이지에
    	- 출력까지 완료

---
+++ lec02_jsp/src/main/java/com/gn/practice/controller/BookReservationController.java
@@ -0,0 +1,59 @@
+package com.gn.practice.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/borrow")
+public class BookReservationController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public BookReservationController() {
+        super();
+        
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String userName = request.getParameter("user_name");
+		String userPhone = request.getParameter("user_phone");
+		String userEmail = request.getParameter("user_email");
+		String book = request.getParameter("book");
+		int rentDate = Integer.parseInt(request.getParameter("rentDate"));
+		
+		int price = 0;
+		String bookName = "";
+		
+		switch(book) {
+			case "1" : price = 1500; bookName = "자바 프로그래밍 입문"; break;
+			case "2" : price = 1800; bookName = "웹 개발의 기초"; break;
+			case "3" : price = 2000; bookName = "데이터베이스의 시스템"; break;
+		}
+		
+		if(rentDate > 1) {
+			price = price + 500*(rentDate-1);
+		}
+		
+		RequestDispatcher view = request.getRequestDispatcher("views/bookConfirmation.jsp");
+		request.setAttribute("name", userName);
+		request.setAttribute("phone", userPhone);
+		request.setAttribute("email", userEmail);
+		request.setAttribute("bookName", bookName);
+		request.setAttribute("rentDate", rentDate);
+		request.setAttribute("price", price);
+		view.forward(request, response);
+		
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ lec02_jsp/src/main/webapp/views/bookReservation.jsp
@@ -0,0 +1,38 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>도서 대출하기</title>
+</head>
+<body>
+	<form method="post" name="bookFrm" id="bookFrm" action="/borrow">
+	<fieldset>
+        <legend>고객 정보</legend>
+        <label for="user_name">고객명 : </label>
+        <input type="text" id="user_name" name="user_name"><br>
+        <label for="user_phone">전화번호 : </label>
+        <input type="text" id="user_phone" name="user_phone"><br>
+        <label for="user_mail">E_mail : </label>
+        <input type="email" id="user_mail" name="user_email">
+    </fieldset>
+    <fieldset>
+        <legend>도서 선택</legend>
+        <label for="book">도서 : </label>
+        <select id="book" name="book">
+            <option value="0">선택</option>
+            <option value="1">자바 프로그래밍 입문</option>
+            <option value="2">웹 개발의 기초</option>
+            <option value="3">데이터베이스 시스템 </option>
+        </select>
+    </fieldset>
+    <fieldset>
+        <legend>대출 기간</legend>
+        <label>대출 기간(일) : </label>
+        <input type="number" name="rentDate">
+    </fieldset>
+    <button>대출하기</button>
+    </form>
+</body>
+</html>
