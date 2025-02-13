3ad5b24bb7805fc10298943bacca7bbb576e82e4
4559jacky <4559jacky@gmail.com>
2025-02-13 10:37:28 +0900

    [jji] 과제 중간 commit

--- lec04_session/src/main/java/com/gn/shopping/controller/ProductListServlet.java
+++ lec04_session/src/main/java/com/gn/shopping/controller/ProductListServlet.java
@@ -1,11 +1,19 @@
 package com.gn.shopping.controller;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.gn.shopping.vo.Product;
 
 @WebServlet("/ProductListServlet")
 public class ProductListServlet extends HttpServlet {
@@ -16,7 +24,30 @@ public class ProductListServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String prodCode = request.getParameter("prod_code");
+		
+		int prodCode = Integer.parseInt(request.getParameter("prod_code"));
+		String prodName = request.getParameter("prod_name");
+		int prodPrice = Integer.parseInt(request.getParameter("prod_price"));
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<Product> list = new ArrayList<Product>();
+		
+		try {
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs != null && !rs.isClosed()) rs.close();
+				if(pstmt != null && !pstmt.isClosed()) rs.close();
+				if(conn != null && !conn.isClosed()) conn.close();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
