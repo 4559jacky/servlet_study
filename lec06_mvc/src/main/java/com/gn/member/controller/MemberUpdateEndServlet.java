d158109c343631c678ec1861b3e77f7dc10be173
4559jacky <4559jacky@gmail.com>
2025-02-14 17:50:17 +0900

    [jji] commit

--- lec06_mvc/src/main/java/com/gn/member/controller/MemberUpdateEndServlet.java
+++ lec06_mvc/src/main/java/com/gn/member/controller/MemberUpdateEndServlet.java
@@ -19,7 +19,7 @@ import com.gn.member.vo.Member;
 @WebServlet(name = "memberUpdateEndServlet", urlPatterns = "/memberUpdateEnd")
 public class MemberUpdateEndServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
 
     public MemberUpdateEndServlet() {
         super();
