7bbe53133f8d2e24267ed14f6cc18fd17edc7276
4559jacky <4559jacky@gmail.com>
2025-02-21 09:26:06 +0900

    [jji] 트랜잭션을 위한 getSqlSession 메소드 오버로드

--- lec08_mybatis/src/main/java/com/gn/common/sql/SqlSessionTemplate.java
+++ lec08_mybatis/src/main/java/com/gn/common/sql/SqlSessionTemplate.java
@@ -29,4 +29,26 @@ public class SqlSessionTemplate {
 		}
 		return session;
 	}
+	
+	// 트랜잭션을 위한 오버로드
+	public static SqlSession getSqlSession(boolean bool) {
+		SqlSession session = null;
+		try {
+			// 1. mybatis-config.xml의 설정 정보 읽어오기
+			String path = "/mybatis-config.xml";
+			InputStream is = Resources.getResourceAsStream(path);
+			// 2. SqlSessionFactoryBuilder 객체 생성
+			SqlSessionFactoryBuilder sfb
+				= new SqlSessionFactoryBuilder();
+			// 3. SqlSessionFactory 객체 생성
+			SqlSessionFactory factory = sfb.build(is);
+			// 4. SqlSession 객체 생성
+			// 매개변수 -> AutoCommit여부 지정 -> default : true
+			// AutoCommit 끌 때 -> false
+			session = factory.openSession(bool);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return session;
+	}
 }
